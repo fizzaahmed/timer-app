4959b12032bf46dcd90e1b6599b5b2d35ad75f9c
Fizza Ahmed <fizza0ahmed@gmail.com>
2024-08-11 13:15:00 -0400

    start all button added

--- App.js
+++ App.js
@@ -1,20 +1,112 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef } from 'react'; 
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, SafeAreaView, TextInput, Button, FlatList }  
+    from 'react-native'; 
+import Stopwatch from './Stopwatch.js';
+  
+const App = () => {  
+    const [numRunners, setNumRunners] = useState(0); 
+    const [runners, setRunners] = useState([]);
+    const [name, setName] = useState('');
+    const [startAll, setStartAll] = useState(false);
+    const STOPWATCH_HEIGHT = 10; 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const addRunner = () => { 
+        if (runners.length == 0){
+            setRunners([name]);
+        }
+        else {
+            const combinedNames = runners.concat([name])
+            setRunners(combinedNames);
+        }
+        setNumRunners(runners.length);
+    };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    const startAllStopwatches = () => {
+        setStartAll(true);
+        setTimeout(() => setStartAll(false), 100);  // reset after a short delay
+    };
+
+
+    return ( 
+        <View style={styles.container}> 
+            <Text style={styles.header}> 
+                Emily's App for Runners 
+            </Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Type new runner name here!"
+                onChangeText={newText => setName(newText)}
+                defaultValue={name}
+            />
+            <TouchableOpacity
+                style={styles.button}
+                onPress={addRunner}
+                title="Add"
+                color="#841584">
+                <Text style={styles.buttonText}>Add</Text> 
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={[styles.button, { marginTop: 10 }]}
+                onPress={startAllStopwatches}
+                title="Start All"
+                color="#841584">
+                <Text style={styles.buttonText}>Start All</Text> 
+            </TouchableOpacity>
+            <SafeAreaView style={{flex: 1}}>        
+                <FlatList
+                    data={runners}
+                    getItemLayout={(item, index) => (
+                        {length: STOPWATCH_HEIGHT, offset: STOPWATCH_HEIGHT * index, index}
+                    )}
+                    renderItem={({ item }) => (
+                        <Stopwatch runnerName={item} startExternally={startAll} />
+                    )}
+                />   
+            </SafeAreaView>
+        </View> 
+    ); 
+}; 
+  
+const styles = StyleSheet.create({ 
+    container: { 
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+    }, 
+    header: { 
+        fontSize: 30, 
+        color: "green", 
+        marginTop: 60,
+    }, 
+    subHeader: { 
+        fontSize: 18, 
+        marginBottom: 10, 
+        color: "blue", 
+    }, 
+    timeText: { 
+        fontSize: 48, 
+    }, 
+    buttonContainer: { 
+        flexDirection: 'row', 
+        marginTop: 20, 
+    }, 
+    button: { 
+        paddingVertical: 10, 
+        paddingHorizontal: 20, 
+        borderRadius: 5, 
+        backgroundColor: '#2ecc71', 
+        marginRight: 10, 
+    },
+    buttonText: { 
+        color: 'white', 
+        fontSize: 16, 
+    }, 
+    input: {
+        margin: 15,
+        height: 40,
+        borderColor: '#7a42f4',
+        borderWidth: 1
+     },
+}); 
+  
+export default App;
---
+++ Stopwatch.js
@@ -0,0 +1,246 @@
+import React, { useState, useRef, useEffect } from 'react'; 
+import { View, Text, StyleSheet, TouchableOpacity, FlatList }  
+    from 'react-native'; 
+
+const timestamp = {
+    minutes: 0,
+    seconds: 0,
+    milliseconds: 0
+}
+  
+const Stopwatch = ({runnerName, startExternally}) => { 
+    // State and refs to manage time and stopwatch status 
+    const [time, setTime] = useState(0); 
+    const [minutes, setMinutes] = useState(0); 
+    const [seconds, setSeconds] = useState(0); 
+    const [centiseconds, setCentiseconds] = useState(0); 
+    //const [timestamp, setTimeStamp] = useState({minutes: 0, seconds: 0, milliseconds: 0});
+    const [running, setRunning] = useState(false);
+    const [lapTimes, setLapTimes] = useState([]);
+    const [haveLapTimes, setHaveLapTimes] = useState(false); 
+    const intervalRef = useRef(null); 
+    const startTimeRef = useRef({minutes: 0, seconds: 0, milliseconds: 0});
+    const LAPTIME_HEIGHT = 10; 
+
+    useEffect(() => {
+        if (startExternally) {
+            startStopwatch();
+        }
+    }, [startExternally]);
+
+    // Function to start the stopwatch 
+    const startStopwatch = () => { 
+        // saving the start time in hundreths of a second
+        startTimeRef.current = Date.now() - time * 10; 
+        // what we are doing here. is running the set time function on an interval, and saving a reference to the interval so that we can stop the funciton when we pause/reset
+        intervalRef.current = setInterval(() => {
+            const thisTime =  Math.floor((Date.now() -  
+            startTimeRef.current) / 10);
+            setTime(thisTime);
+            const thisMinutes = Math.floor(thisTime / 6000);
+            setMinutes(thisMinutes);
+            const thisSeconds = Math.floor((thisTime - thisMinutes * 6000) / 100);
+            setSeconds(thisSeconds);
+            setCentiseconds((thisTime - thisMinutes * 6000) - thisSeconds * 100);
+        }, 10); // changing from  1000 to 10 to change interval every centisecond
+        setRunning(true); 
+    }; 
+    // Function to pause the stopwatch 
+    const pauseStopwatch = () => { 
+        clearInterval(intervalRef.current); 
+        setRunning(false);
+    }; 
+
+    // Function to lap the stopwatch
+    const lapStopwatch = () => {
+        const currentTime = minutes + ":" + seconds + ":" + centiseconds;
+        if (lapTimes.length == 0){
+            setLapTimes([currentTime]);
+        }
+        else {
+            const combinedTimes = lapTimes.concat([currentTime])
+            setLapTimes(combinedTimes);
+        }    
+        setHaveLapTimes(true);
+    };
+
+    // Function to reset the stopwatch 
+    const resetStopwatch = () => { 
+        clearInterval(intervalRef.current); 
+        setTime(0); 
+        setMinutes(0);
+        setSeconds(0);
+        setCentiseconds(0);
+        setRunning(false); 
+        setLapTimes([]);
+        setHaveLapTimes(false);
+    }; 
+    // Function to resume the stopwatch 
+    const resumeStopwatch = () => { 
+        startTimeRef.current = Date.now() - time * 1000; 
+        intervalRef.current = setInterval(() => {
+            const thisTime =  Math.floor((Date.now() -  
+            startTimeRef.current) / 10);
+            setTime(thisTime);
+            const thisMinutes = Math.floor(thisTime / 6000);
+            setMinutes(thisMinutes);
+            const thisSeconds = Math.floor(time - thisMinutes * 6000);
+            setSeconds(thisSeconds);
+            setCentiseconds(Math.floor((time - thisMinutes * 6000) - thisSeconds * 100));
+        }, 10); 
+        setRunning(true); 
+    }; 
+  
+    return ( 
+        <View style={styles.container}>
+            <View style={styles.runnerTimeContainer}>  
+                <Text>Runner: {runnerName}</Text> 
+                <Text style={styles.timeText}>{minutes}:{seconds}:{centiseconds}</Text>
+            </View>
+            <View style={styles.lapTimesContainer}>
+                {haveLapTimes ? (
+                <FlatList
+                data={lapTimes}
+                getItemLayout={(laptime, index) => (
+                    {length: LAPTIME_HEIGHT, offset: LAPTIME_HEIGHT * index, index}
+                  )}
+                keyExtractor={(laptime, index) => index.toString()}
+                renderItem={({ item, index }) => (
+                    <View style={styles.itemContainer}>
+                    <Text style={styles.laptime}>Lap {index + 1}: {item.toString()}</Text>
+                    </View>
+                )}
+                />)
+                :
+                (<></>)
+                }
+            </View>
+            
+            <View style={styles.buttonContainer}> 
+                {running ? ( 
+                    <>
+                    <TouchableOpacity 
+                        style={[styles.button, styles.pauseButton]} 
+                        onPress={pauseStopwatch} 
+                    > 
+                        <Text style={styles.buttonText}>Pause</Text> 
+                    </TouchableOpacity> 
+                    <TouchableOpacity 
+                    style={[styles.button, styles.lapButton]} 
+                    onPress={lapStopwatch} 
+                    > 
+                    <Text style={styles.buttonText}>Lap</Text> 
+                    </TouchableOpacity>
+                    </>
+                    
+                ) : ( 
+                    <> 
+                        <TouchableOpacity 
+                            style={[styles.button, styles.startButton]} 
+                            onPress={startStopwatch} 
+                        > 
+                            <Text style={styles.buttonText}>Start</Text> 
+                        </TouchableOpacity> 
+                        <TouchableOpacity 
+                            style={[styles.button, styles.resetButton]} 
+                            onPress={resetStopwatch} 
+                        > 
+                            <Text style={styles.buttonText}> 
+                                Reset 
+                            </Text> 
+                        </TouchableOpacity> 
+                    </> 
+                )} 
+                {!running && ( 
+                    <TouchableOpacity 
+                        style={[styles.button, styles.resumeButton]} 
+                        onPress={resumeStopwatch} 
+                    > 
+                        <Text style={styles.buttonText}> 
+                            Resume 
+                        </Text> 
+                    </TouchableOpacity> 
+                )} 
+            </View> 
+        </View> 
+    ); 
+}; 
+  
+const styles = StyleSheet.create({ 
+    container: { 
+        flex: 1, 
+    }, 
+    header: { 
+        fontSize: 30, 
+        color: "green", 
+        marginBottom: 10, 
+    }, 
+    subHeader: { 
+        fontSize: 18, 
+        marginBottom: 10, 
+        color: "blue", 
+        justifyContent: 'left', 
+        alignItems: 'left', 
+    }, 
+    buttonContainer: { 
+        flexDirection: 'row', 
+        marginTop: 20, 
+        justifyContent: 'right', 
+        alignItems: 'right', 
+    },
+    runnerTimeContainer: {
+        flexDirection: 'row',
+        marginTop: 20, 
+        justifyContent: 'space-between', // Distribute space between elements
+        width: '80%', // Ensure the container takes full width
+    },
+    lapTimesContainer: {
+        flexShrink: 1
+    }, 
+    itemContainer: {
+        flexDirection: 'row',
+        padding: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+      },
+      index: {
+        marginRight: 10,
+        fontWeight: 'bold',
+      },
+      laptimeT: {
+        flex: 1,
+      },
+    timeText: { 
+        fontSize: 24,
+    }, 
+    button: { 
+        paddingVertical: 10, 
+        paddingHorizontal: 20, 
+        borderRadius: 5, 
+    }, 
+    startButton: { 
+        backgroundColor: '#2ecc71', 
+        marginRight: 10, 
+    }, 
+    resetButton: { 
+        backgroundColor: '#e74c3c', 
+        marginRight: 10, 
+    }, 
+    pauseButton: { 
+        backgroundColor: '#f39c12', 
+        marginRight: 10,
+    }, 
+    resumeButton: { 
+        backgroundColor: '#3498db', 
+    },
+    lapButton: { 
+        backgroundColor: '#40E0D0', 
+        marginLeft: 10,
+    }, 
+    buttonText: { 
+        color: 'white', 
+        fontSize: 16, 
+    }, 
+}); 
+  
+export default Stopwatch;
